0b22305d15362d4ed8e75e067d600eedd87434ff
Uzair Ahmed <uzairmahmed@gmail.com>
2021-01-17 08:26:57 -0500

    things work!

--- backend/server/main.py
+++ backend/server/main.py
@@ -67,5 +67,5 @@ def checkEveryItemForEveryUser():
                 
         smsRemind(username, expiredList, timeUntilExp.days)
 
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+# if __name__ == '__main__':
+#     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
